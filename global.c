88b6dae0f2563bdd1567784fd2f7c9083c6b756e
www176626 <wh176626@gmail.com>
2025-05-25 18:47:42 +0800

    test

--- global.c
+++ global.c
@@ -11,5 +11,5 @@ ALLEGRO_EVENT event;
 ALLEGRO_TIMER *fps = NULL;
 bool key_state[ALLEGRO_KEY_MAX] = {false};
 bool mouse_state[ALLEGRO_MOUSE_MAX_EXTRA_AXES] = {false};
-Point mouse;
+Point mouse;??
 bool debug_mode = true;
